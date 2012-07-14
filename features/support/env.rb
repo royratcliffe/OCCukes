7fec07343b73cfd66044856caef7276689840a5b
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-07-14 08:29:53 +0100

    Adjusted and expanded comments

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,20 @@
-# Navigate to the wire language configuration. Cucumber supports
-# multiple languages, even during the same run. The wire language is
-# just one of many. No straightforward way exists for accessing the
-# current language, or even the current runtime from within Cucumber
-# step definition Ruby blocks.
-#
-# No need to send an exit message. The wire server automatically exits
-# when all the connections close.
 AfterConfiguration do |config|
+  # First, daemonise this Cucumber process. This assumes that Xcode
+  # launches Cucumber as a pre-action for the test scheme. If you use
+  # RVM, the pre-action script might look something like this:
+  #
+  #   cd "$SRCROOT"
+  #   "$HOME/.rvm/bin/rvm-shell" -c "cucumber"
+  #
   Process.daemon(true, true)
+
+  # Navigate to the wire language configuration. Cucumber supports
+  # multiple languages, even during the same run. The wire language is
+  # just one of many. No straightforward way exists for accessing the
+  # current language, or even the current runtime from within Cucumber
+  # at this point during the AfterConfiguration block. Instead
+  # therefore, replicate Cucumber's way of finding and loading the
+  # wire configuration.
   feature_dirs = ['features'].map { |f| File.directory?(f) ? f : File.dirname(f) }.uniq
   wire_files = feature_dirs.map do |path|
     path = path.gsub(/\/$/, '')
@@ -16,6 +23,15 @@ AfterConfiguration do |config|
   wire_files.reject! { |f| !File.file?(f) }
   wire_files.reject! { |f| File.extname(f) != '.wire' }
   params = YAML.load(ERB.new(File.read(wire_files[0])).result)
+
+  # Finally, wait for the wire socket to open. Try a connection once a
+  # second for ten seconds. Continue when the connection does not
+  # refuse. This adds a short latency: the distance in time between
+  # the wire server accepting connections and the socket probe finding
+  # a non-refusal. The latency is always less than one second.
+  #
+  # No need to send an exit message. The wire server automatically exits
+  # when all the connections close.
   Timeout.timeout(10) do
     loop do
       begin
