fc6a4c48b6a2ee797dfbf0f9c0f4b38c7b7b9bb9
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-07-26 13:59:30 +0100

    Make an immutable copy of the snippet pattern for the result

--- OCCukes/OCCucumberRuntime+WireProtocol.m
+++ OCCukes/OCCucumberRuntime+WireProtocol.m
@@ -142,7 +142,7 @@ NSString *__OCCucumberRuntimeCamelize(NSString *string);
 	[snippetPattern replaceOccurrencesOfString:@"\\" withString:@"\\\\" options:0 range:NSMakeRange(0, [snippetPattern length])];
 	[snippetPattern replaceOccurrencesOfString:@"\"" withString:@"\\\"" options:0 range:NSMakeRange(0, [snippetPattern length])];
 	
-	return [NSArray arrayWithObjects:@"success", [NSString stringWithFormat:@"\t[OCCucumber %@:@\"^%@$\" step:^(NSArray *arguments) {\n\t\t// express the regular expression above with the code you wish you had\n\t\t[OCCucumber pending:@\"TODO\"];\n\t} file:__FILE__ line:__LINE__];", [stepKeyword lowercaseString], snippetPattern], nil];
+	return [NSArray arrayWithObjects:@"success", [NSString stringWithFormat:@"\t[OCCucumber %@:@\"^%@$\" step:^(NSArray *arguments) {\n\t\t// express the regular expression above with the code you wish you had\n\t\t[OCCucumber pending:@\"TODO\"];\n\t} file:__FILE__ line:__LINE__];", [stepKeyword lowercaseString], [snippetPattern copy]], nil];
 }
 
 - (id)handleBeginScenario
