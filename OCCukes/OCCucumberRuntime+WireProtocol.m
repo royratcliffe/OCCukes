418a9cd47ff8de93fdd95c33392fec00520e21ab
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-07-26 12:24:37 +0100

    Escape backslashes and quotes in snippet patterns

--- OCCukes/OCCucumberRuntime+WireProtocol.m
+++ OCCukes/OCCucumberRuntime+WireProtocol.m
@@ -139,6 +139,8 @@ NSString *__OCCucumberRuntimeCamelize(NSString *string);
 		NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:&error];
 		[regularExpression replaceMatchesInString:snippetPattern options:0 range:NSMakeRange(0, [snippetPattern length]) withTemplate:[NSRegularExpression escapedPatternForString:pattern]];
 	}
+	[snippetPattern replaceOccurrencesOfString:@"\\" withString:@"\\\\" options:0 range:NSMakeRange(0, [snippetPattern length])];
+	[snippetPattern replaceOccurrencesOfString:@"\"" withString:@"\\\"" options:0 range:NSMakeRange(0, [snippetPattern length])];
 	
 	return [NSArray arrayWithObjects:@"success", [NSString stringWithFormat:@"\t[OCCucumber %@:@\"^%@$\" step:^(NSArray *arguments) {\n\t\t// express the regular expression above with the code you wish you had\n\t\t[OCCucumber pending:@\"TODO\"];\n\t} file:__FILE__ line:__LINE__];", [stepKeyword lowercaseString], snippetPattern], nil];
 }
